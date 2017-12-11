3be194b24a67cd381d53068ebdfe754409ef4bb9
MarkusG <markus.giay@gmail.com>
2017-12-11 16:57:44 +0100

    init commit

---
+++ MarkusG/di-example/di-example/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace di_example
+{
+    public class HelloController
+    {
+        private readonly IHelloService _service;
+
+        public HelloController(IHelloService service)
+        {
+            _service = service;
+        }
+
+        public string Greeting(string name)
+        {
+            return _service.Hello(name);
+        }
+    }
+}
