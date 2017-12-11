3d2a717cca8edfbb856beee34f02463cae90fbc3
Sabine Döbrössy <sabine.dobrossy@tieto.com>
2017-12-11 16:55:47 +0100

    console app

---
+++ Sabine/ConsoleApp1/ConsoleApp1/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class HelloController
+    {
+        private readonly IHelloService _helloService;
+        public HelloController(IHelloService helloService)
+        {
+            _helloService = helloService;
+        }
+
+        public string Greeting(string name)
+        {
+            return _helloService.Hello(name);
+        }
+    }
+}
---
+++ Sabine/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static ServiceProvider AppServices { get; private set; }
+
+        static void Main(string[] args)
+        {
+            ConfigureService();
+            RunWithAContainer();
+            //Console.WriteLine("Hello World!");
+        }
+
+        private static void RunWithAContainer()
+        {
+            var helloController = AppServices.GetRequiredService<HelloController>();
+            string greet = helloController.Greeting("Katharina");
+            Console.WriteLine(greet);
+        }
+        private static void ConfigureService()
+        {
+            var services = new ServiceCollection();
+            services.AddSingleton<IHelloService, HelloService>();
+            services.AddTransient<HelloController>();
+
+            AppServices = services.BuildServiceProvider();
+        }
+    }
+}
