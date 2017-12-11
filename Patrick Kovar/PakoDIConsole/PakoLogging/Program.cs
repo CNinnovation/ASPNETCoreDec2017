0029739b43ca908b87ca59038b7c308fab8b1dd8
VS17 Dev <vs17_dev@outlook.com>
2017-12-11 16:51:34 +0100

    asdf

--- Patrick Kovar/PakoDIConsole/PakoLogging/Program.cs
+++ Patrick Kovar/PakoDIConsole/PakoLogging/Program.cs
@@ -1,17 +1,44 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace PakoLogging
 {
     class Program
     {
+        public static IServiceProvider AppServices { get; private set; }
+        private static readonly ILogger<Program> log;
+
         static void Main(string[] args)
         {
+            Configure();
+            var y = new LoggerFactory();
+            log = y.CreateLogger<Program>();
+            var x  = AppServices.GetRequiredService;
+            
             MethodWithLogging();
+
+           
+
+            Console.ReadLine();
+        }
+
+        private static void Configure()
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(options =>
+            {
+                options.AddConsole();
+            });
+            AppServices = services.BuildServiceProvider();
         }
 
         private static void MethodWithLogging()
         {
             
+
+            log.LogInformation("Bar started");
         }
     }
 }
