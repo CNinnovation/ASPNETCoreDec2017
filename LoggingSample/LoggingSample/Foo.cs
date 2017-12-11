6fa72a210c4ae9f5af769d8293b5506ad4e0fc00
Christian Nagel <christian@christiannagel.com>
2017-12-11 14:25:38 +0100

    logging

---
+++ LoggingSample/LoggingSample/Foo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoggingSample
+{
+    public class Foo
+    {
+        private readonly ILogger<Foo> log;
+        public Foo(ILoggerFactory logger)
+        {
+            log = logger.CreateLogger<Foo>();
+        }
+
+        public void Bar()
+        {
+            log.LogInformation("Bar started");
+            Console.WriteLine("bar");
+            log.LogError("some error in Bar");
+        }
+    }
+}
---
+++ LoggingSample/LoggingSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace LoggingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigureServices();
+            var foo = AppServices.GetService<Foo>();
+            foo.Bar();
+            Console.ReadLine();
+        }
+
+        private static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<Foo>();
+            services.AddLogging(options =>
+            {
+                options.AddConsole();
+                options.AddDebug();
+            });
+            AppServices = services.BuildServiceProvider();
+        }
+        public static IServiceProvider AppServices { get; private set; }
+    }
+}
