fd5acb8eace43805a9b81406fa8c4e295ecdd5af
Christian Nagel <christian@christiannagel.com>
2017-12-12 16:49:59 +0100

    ASP.NET Core MVC sample

---
+++ MyMVCWebApp/MyMVCWebApp/Controllers/CalcController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMVCWebApp.Controllers
+{
+    public class CalcController
+    {
+        public int Add(int x, int y) => x + y;
+        public int Subtract(int x, int y) => x - y;
+        public int Multiply(int x, int y) => x * y;
+
+        public string Demo() => $"Demo von CalcController";
+    }
+}
---
+++ MyMVCWebApp/MyMVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() => "Hello, ASP.NET Core MVC";
+
+        public string Demo() => "Demo";
+
+        public string WithId(string id) => $"received id: {id}";
+    }
+}
