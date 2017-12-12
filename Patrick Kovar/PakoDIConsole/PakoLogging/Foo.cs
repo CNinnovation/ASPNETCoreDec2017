4195d1e575c8d21ba845ad3c901adca7895b637c
VS17 Dev <vs17_dev@outlook.com>
2017-12-12 09:39:34 +0100

    asdf

--- Patrick Kovar/PakoDIConsole/PakoLogging/Foo.cs
+++ Patrick Kovar/PakoDIConsole/PakoLogging/Foo.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace PakoLogging
 {
-    class Foo
+    public class Foo
     {
+        public ILogger myLogger{ get; set; }
+        public Foo(ILoggerFactory loggerFactory)
+        {
+            myLogger = loggerFactory.CreateLogger<Foo>();
+        }
+
+        public void LogSth()
+        {
+            myLogger.LogError("mei error");
+        }
     }
 }
--- Patrick Kovar/PakoDIConsole/PakoLogging/Program.cs
+++ Patrick Kovar/PakoDIConsole/PakoLogging/Program.cs
@@ -7,38 +7,27 @@ namespace PakoLogging
     class Program
     {
         public static IServiceProvider AppServices { get; private set; }
-        private static readonly ILogger<Program> log;
+        private  readonly ILogger<Program> log;
+
+     
 
         static void Main(string[] args)
         {
             Configure();
-            var y = new LoggerFactory();
-            log = y.CreateLogger<Program>();
-            var x  = AppServices.GetRequiredService;
-            
-            MethodWithLogging();
-
-           
-
+            var x = AppServices.GetService<Foo>();
+            x.LogSth();
             Console.ReadLine();
         }
 
         private static void Configure()
         {
             var services = new ServiceCollection();
-
+            services.AddTransient<Foo>();
             services.AddLogging(options =>
             {
                 options.AddConsole();
             });
             AppServices = services.BuildServiceProvider();
-        }
-
-        private static void MethodWithLogging()
-        {
-            
-
-            log.LogInformation("Bar started");
         }
     }
 }
