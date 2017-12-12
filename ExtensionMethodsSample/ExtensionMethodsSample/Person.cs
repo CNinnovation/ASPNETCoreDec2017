b2281f6ad0813f1b2776d5e2b5066db09e21dee8
Christian Nagel <christian@christiannagel.com>
2017-12-12 14:15:42 +0100

    extension methods

---
+++ ExtensionMethodsSample/ExtensionMethodsSample/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegatesIrInterfaces
+{
+    public class Person : IComparable<Person>
+    {
+        public string FirstName { get; set; }
+        private string _lastName;
+
+        public string LastName
+        {
+            get => _lastName;
+            set => _lastName = value;
+        }
+
+
+        public int CompareTo(Person other) => this.LastName.CompareTo(other.LastName);
+
+
+        public override string ToString() => LastName;
+
+    }
+}
---
+++ ExtensionMethodsSample/ExtensionMethodsSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DelegatesIrInterfaces
+{
+    static class CollectionExtensions
+    {
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> source, Func<T, bool> pred)
+        {
+            foreach (T item in source)
+            {
+                if (pred(item))
+                    yield return item;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person { FirstName = "Jochen", LastName = "Rindt"},
+                new Person { FirstName = "Niki", LastName = "Lauda"},
+                new Person { FirstName = "Juan Manuel", LastName = "Fangio"},
+                new Person { FirstName = "Gerhard", LastName = "Berger"},
+                new Person { FirstName = "Alex", LastName = "Wurz"},
+                new Person { FirstName = "James", LastName = "Hunt"}
+            };
+
+
+            var jRacers = people.Where(p => p.FirstName.StartsWith("J")).ToList();
+            people.Add(new Person { FirstName = "Jack", LastName = "Brabham" });
+            foreach (var r in jRacers)
+            {
+                Console.WriteLine(r);
+            }
+
+            var orderedPeople = people.OrderBy(p => p.LastName);
+
+        }
+    }
+}
