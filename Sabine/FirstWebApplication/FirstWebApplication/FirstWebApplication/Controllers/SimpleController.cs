5ca4d42c15071108095b6fe308a8511ef1511ba2
Sabine Döbrössy <sabine.dobrossy@tieto.com>
2017-12-12 16:11:59 +0100

    first web app

---
+++ Sabine/FirstWebApplication/FirstWebApplication/FirstWebApplication/Controllers/SimpleController.cs
@@ -0,0 +1,23 @@
+﻿using FirstWebApplication.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication.Controllers
+{
+    public class SimpleController 
+    {
+        private readonly IGreetingService _greetingService;
+
+        public SimpleController (IGreetingService greetingService)
+        {
+            _greetingService = greetingService;
+        }
+
+        public string Hello(string name)
+        {
+            return _greetingService.Greet(name);
+        }
+    }
+}
---
+++ Sabine/FirstWebApplication/FirstWebApplication/FirstWebApplication/Services/IGreetingService.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstWebApplication.Services
+{
+    public interface IGreetingService
+    {
+        string Greet(string name);
+    }
+}
