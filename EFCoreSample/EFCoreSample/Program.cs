3963aba9351a07902a15d7b20d946c3755b9295b
Christian Nagel <christian@christiannagel.com>
2017-12-13 17:04:49 +0100

    EF Core sample

---
+++ EFCoreSample/EFCoreSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EFCoreSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDatabase();
+            AddRecords();
+        }
+
+        private static void CreateDatabase()
+        {
+            using (var context = new BooksContext())
+            {
+                bool created = context.Database.EnsureCreated();
+                Console.WriteLine($"database created: {created}");
+            }
+        }
+
+        private static void AddRecords()
+        {
+            using (var context = new BooksContext())
+            {
+                context.Books.Add(new Book { Title = "Professional C# 6", Publisher = "Wrox Press" });
+                int recordschanged = context.SaveChanges();
+                Console.WriteLine($"records changed: {recordschanged}");
+            }
+        }
+    }
+}
