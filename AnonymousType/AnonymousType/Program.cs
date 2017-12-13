23f3a66a216ecb6a0b221f8a736a1fa9d36c642b
Christian Nagel <christian@christiannagel.com>
2017-12-13 13:29:16 +0100

    anonymous types

---
+++ AnonymousType/AnonymousType/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnonymousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new { FirstName = "Tom", LastName = "Turbo" };
+            Console.WriteLine(x.GetType().Name);
+        }
+    }
+}
