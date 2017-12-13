377cc2df9851dc77e67d747cfd887b67f8efb60d
Christian Nagel <christian@christiannagel.com>
2017-12-13 17:04:29 +0100

    scaffolding

---
+++ ScaffoldingSample/ScaffoldingSample/Books.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScaffoldingSample
+{
+    [Table("Books", Schema = "books")]
+    public partial class Books
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(25)]
+        public string Publisher { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+        [StringLength(20)]
+        public string Isbn { get; set; }
+    }
+}
---
+++ ScaffoldingSample/ScaffoldingSample/BooksSample2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ScaffoldingSample
+{
+    public partial class BooksSample2Context : DbContext
+    {
+        public virtual DbSet<Books> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"server=(localdb)\mssqllocaldb;database=BooksSample2;trusted_connection=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
