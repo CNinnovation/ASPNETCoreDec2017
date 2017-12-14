04ecc72e1d3a0efdacf1bc495597dcde82ba0b9e
Christian Nagel <christian@christiannagel.com>
2017-12-14 17:27:08 +0100

    send XML or JSON

--- 05_API/MVCWithEFCore/MVCWithEFCore/Startup.cs
+++ 05_API/MVCWithEFCore/MVCWithEFCore/Startup.cs
@@ -25,7 +25,7 @@ namespace MVCWithEFCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddXmlSerializerFormatters();
+            services.AddMvc(options => options.RespectBrowserAcceptHeader = true).AddXmlSerializerFormatters();
             services.AddDbContext<BooksContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("BooksConnection"));
